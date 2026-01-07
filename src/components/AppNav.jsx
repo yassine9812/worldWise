b948aa183654c016ab97b3b6dfdb0e218244cb63
yassine9812 <yassine50.zayet@gmail.com>
2026-01-07 18:41:55 +0100

    Building app layout

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./AppNav.module.css";
 
 export default function AppNav() {
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import AppNav from '../components/AppNav'
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
+import Map from "../components/Map";
 
 export default function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>App Layout</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
-  )
+  );
 }
